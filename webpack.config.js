3180dd55e078945b76fb6bd2ee043a07d673b00a
Gauguin94 <rhrud0303@gmail.com>
2022-06-11 22:08:18 +0900

    edit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var webpack = require("webpack"),
+    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    HtmlPlugin = require("./plugins/html-plugin"),
+    path = require("path");
+
+var cssExtractTextPlugin = new ExtractTextPlugin("[name].css");
+
+module.exports = {
+  devServer: {
+    port: 9000,
+    historyApiFallback: true,
+  },
+
+  entry: {
+    "script": "./scripts/index.js",
+    "style": "./styles/index.less",
+  },
+
+  module: {
+    rules: [
+      { test: /\.txt$/, loader: "text-loader" },
+      { test: /\.json$/, loader: "json-loader"},
+      { test: /\.js$/, exclude: /(node_modules|bower_components)\//, loader: "babel-loader"},
+      { test: /\.(ttf.*|eot.*|woff.*|ogg|mp3)$/, loader: "file-loader"},
+      { test: /.(png|jpe?g|gif|svg.*)$/, loader: "file-loader"},
+      {
+        test: /\.css$/,
+        loader: cssExtractTextPlugin.extract("css-loader"),
+      },
+      {
+        test: /\.less$/,
+        loader: cssExtractTextPlugin.extract("css-loader!less-loader"),
+      },
+    ],
+  },
+
+  plugins: [
+    cssExtractTextPlugin,
+    new HtmlPlugin("index.html"),
+    new webpack.DefinePlugin({
+      CONFIG: JSON.stringify(require("config")),
+    }),
+  ],
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, "src"),
+      path.resolve(__dirname, "node_modules"),
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+};
